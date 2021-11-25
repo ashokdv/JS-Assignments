da4f9b8439a1c62185bdeb0c99fe9b67bd2b7b3d
ashokdv <13602@ramco.com>
2021-11-25 19:42:38 +0530

    favourite_movie

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,18 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+
+    if(operation ==='add'){
+        movies.push(movie);
+        // console.log(movies);
+    }
+    else if(operation ==='remove'){
+        movies.pop();
+        // console.log(movies);
+    }
+
+    return movies
+
 }
 
 module.exports = favouriteMovie;
